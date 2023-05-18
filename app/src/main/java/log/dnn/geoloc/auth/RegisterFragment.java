f9e613dc9be9171f37b1c451ec75886d0f2ac54f
Dawil_Log <willnono8@gmail.com>
2023-05-18 17:05:44 +0200

    Login Register

---
+++ app/src/main/java/log/dnn/geoloc/auth/RegisterFragment.java
@@ -0,0 +1,25 @@
+package log.dnn.geoloc.auth;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import log.dnn.geoloc.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
