cac13ef4166161e3e2091f927b08b6d60c802a13
Dawil_Log <willnono8@gmail.com>
2023-05-18 07:30:06 +0200

    Swip for award swip pour avancer

--- app/src/main/java/log/dnn/geoloc/OnBoardingActivity.java
+++ app/src/main/java/log/dnn/geoloc/OnBoardingActivity.java
@@ -1,14 +1,23 @@
 package log.dnn.geoloc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
+    public NavHostController navHostController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+
+        navHostController = (NavHostController) navHostFragment.getNavController();
     }
 }
