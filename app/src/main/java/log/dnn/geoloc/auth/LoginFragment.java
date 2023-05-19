e3f8e0d537912921ca44a1470746e427642cd15a
DAWIL-LOG <84101938+DAWIL-LOG@users.noreply.github.com>
2023-05-19 20:49:20 +0200

    wd,bjjgxdvjkdfhjkdv

--- app/src/main/java/log/dnn/geoloc/auth/LoginFragment.java
+++ app/src/main/java/log/dnn/geoloc/auth/LoginFragment.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import log.dnn.geoloc.R;
 
@@ -14,12 +20,65 @@ import log.dnn.geoloc.R;
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
+
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private Button btnSignIn;
+    private TextView txtError;
+    private String messageError;
+
+    private String[][] USERS = {{"willnono@gmail.com", "12345"},{"ngahemeniw@gmail.com", "12345"}};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        txtEmail = view.findViewById(R.id.txt_email);
+        txtPassword = view.findViewById(R.id.txt_password);
+        btnSignIn = view.findViewById(R.id.btn_signin);
+        txtError = view.findViewById(R.id.txt_error);
+
+        btnSignIn.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(isValidated()){
+
+        }else{
+            txtError.setVisibility(TextView.VISIBLE);
+            txtError.setText(messageError);
+        }
+    }
+
+    private boolean isValidated() {
+        if(txtEmail.getText().length()== 0 && txtPassword.getText().length()==0){
+            messageError = MessageError.LOGIN_REQUIRED;
+            return true;
+        }
+        else {
+            if (isFound(txtEmail.getText().toString(), txtPassword.getText().toString())){
+                messageError = "it is OK";
+                return true;
+            }else{
+                messageError = MessageError.LOGIN_USER_NOT_FOUND;
+                return false;
+            }
+
+        }
+
+    }
+
+    private boolean isFound(String email, String password) {
+        for (String[] user:USERS) {
+            if(email == user[0] && password == user[1])
+                return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/log/dnn/geoloc/auth/MessageError.java
@@ -0,0 +1,11 @@
+package log.dnn.geoloc.auth;
+
+public class MessageError {
+
+    public static String LOGIN_REQUIRED = "Email and Password are Required";
+    public static String LOGIN_USER_NOT_FOUND = "Account are not found";
+
+    public static String REGISTER_REQUIRED = "Email and Password are Required";
+    public static String REGISTER_EMAIL_NOT_MATCH = "Email format is incorrect";
+    public static String REGISTER_PASSWORD_NOT_MATCH = "Password";
+}
