b2146a76ba4a54692fdbf5eb1c484ad72257a3f0
DAWIL-LOG <84101938+DAWIL-LOG@users.noreply.github.com>
2023-05-19 10:14:51 +0200

    Onboarding 3 mdif

--- app/src/main/java/log/dnn/geoloc/AuthActivity.java
+++ app/src/main/java/log/dnn/geoloc/AuthActivity.java
@@ -1,14 +1,20 @@
 package log.dnn.geoloc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
 public class AuthActivity extends AppCompatActivity {
 
+    public NavHostController navHostController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_auth);
+        navHostController = (NavHostController) navHostFragment.getNavController();
     }
 }
--- app/src/main/java/log/dnn/geoloc/onboarding/FragmentThree.java
+++ app/src/main/java/log/dnn/geoloc/onboarding/FragmentThree.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import log.dnn.geoloc.OnBoardingActivity;
 import log.dnn.geoloc.OnSwipeListener;
@@ -18,17 +19,26 @@ import log.dnn.geoloc.R;
  */
 public class FragmentThree extends Fragment {
 
+    private Button button_end_onboarding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_on_boarding_three, container, false);
-        view.setOnTouchListener(new OnSwipeListener(getContext()){
+
+        this.button_end_onboarding = view.findViewById(R.id.btnendonboarding);
+
+        this.button_end_onboarding.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSwipeLeft() {
+            public void onClick(View view) {
+
                 ((OnBoardingActivity)getActivity()).navHostController.navigate(R.id.action_fragmentThree_to_mainActivity);
-                ((OnBoardingActivity)getActivity()).finish();
+                getActivity().finish();
             }
+        });
+
+
+        view.setOnTouchListener(new OnSwipeListener(getContext()){
 
             @Override
             public void onSwipeRight() {
